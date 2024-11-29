6426d630fb7c7406d10aa5b824538f782e6ca7af
Navalex <alexnava@hotmail.fr>
2024-11-29 01:43:05 +0100

    Add menu

--- scripts/procedural_map.gd
+++ scripts/procedural_map.gd
@@ -4,38 +4,45 @@ extends Node2D
 
 var noise: Noise
 
-var width: int = 200
-var height: int = 200
+var width: int = 190
+var height: int = 108
 
 var offset = Vector2i(0, 0)
 
 @onready var tile_map = $TileMap
-
 @onready var tile_set = tile_map.tile_set
+
 var water_atlas = 17
 
 func _ready():
 	noise = noise_height_text.noise
 	generate_map()
-	
+
 func generate_map():
-	var grass_water = []
-	var grass_hill = []
-	for x in range(-width/2, width/2):
-		for y in range(-height/2, height/2):
+	# Utiliser des tableaux standards
+	var grass_water: Array[Vector2i] = []
+	var grass_hill: Array[Vector2i] = []
+
+	# Précalculer les limites
+	var start_x = -int(width / 2)
+	var end_x = int(width / 2)
+	var start_y = -int(height / 2)
+	var end_y = int(height / 2)
+
+	for x in range(start_x, end_x):
+		for y in range(start_y, end_y):
+			var position = Vector2i(x, y) + offset
 			var noise_val = noise.get_noise_2d(x, y)
-			var is_grass = false
-			
-			# Set water anyway
-			tile_map.set_cell(0, Vector2i(x, y) +  offset, water_atlas, Vector2i(0, 0))
-			
+
+			# Placer l'eau
+			tile_map.set_cell(0, position, water_atlas, Vector2i(0, 0))
+
+			# Déterminer les types de terrain
 			if noise_val >= 0.0:
-				is_grass = true
-				grass_water.append(Vector2i(x, y) + offset)
-				
-			if noise_val >= 0.1 && is_grass:
-				grass_hill.append(Vector2i(x, y) + offset)
-			
-	
+				grass_water.append(position)
+			if noise_val >= 0.1:
+				grass_hill.append(position)
+
+	# Utiliser les appels de batch pour les terrains
 	tile_map.set_cells_terrain_connect(1, grass_water, 0, 0)
 	tile_map.set_cells_terrain_connect(2, grass_hill, 0, 1)
