741c70a6ab8c7affc0e6007f493747f2aaf478df
Navalex <alexnava@hotmail.fr>
2024-11-30 04:22:51 +0100

    Menu + Chunk load map

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,14 @@ const SPEED = 300.0
 # Référence au AnimatedSprite2D
 @onready var animation_player = $AnimatedSprite2D
 
+# Reference to Camera2D
+@onready var camera: Camera2D = $Camera2D
+
+# Zoom configuration
+@export var min_zoom: float = 0.5
+@export var max_zoom: float = 3.0
+@export var zoom_step: float = 0.1
+
 # Stocker la dernière direction
 var last_direction = "down"
 
@@ -15,22 +23,22 @@ func _physics_process(delta: float) -> void:
 	var direction = Vector2.ZERO
 
 	# Gérer l'entrée utilisateur
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("move_up"):
 		active_directions.append("up")
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_just_pressed("move_down"):
 		active_directions.append("down")
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_just_pressed("move_left"):
 		active_directions.append("left")
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_just_pressed("move_right"):
 		active_directions.append("right")
 
-	if Input.is_action_just_released("ui_up"):
+	if Input.is_action_just_released("move_up"):
 		active_directions.erase("up")
-	if Input.is_action_just_released("ui_down"):
+	if Input.is_action_just_released("move_down"):
 		active_directions.erase("down")
-	if Input.is_action_just_released("ui_left"):
+	if Input.is_action_just_released("move_left"):
 		active_directions.erase("left")
-	if Input.is_action_just_released("ui_right"):
+	if Input.is_action_just_released("move_right"):
 		active_directions.erase("right")
 
 	# Déterminer la direction en fonction de la dernière action active
@@ -60,3 +68,17 @@ func _physics_process(delta: float) -> void:
 	else:
 		animation_player.animation = "idle_" + last_direction
 		animation_player.play()
+	
+	handle_zoom()
+
+
+func handle_zoom():
+	# Zoom in and out using mouse wheel
+	if Input.is_action_just_pressed("scroll_down"):
+		camera.zoom -= Vector2(zoom_step, zoom_step)
+		camera.zoom.x = clamp(camera.zoom.x, min_zoom, max_zoom)
+		camera.zoom.y = clamp(camera.zoom.y, min_zoom, max_zoom)
+	elif Input.is_action_just_pressed("scroll_up"):
+		camera.zoom += Vector2(zoom_step, zoom_step)
+		camera.zoom.x = clamp(camera.zoom.x, min_zoom, max_zoom)
+		camera.zoom.y = clamp(camera.zoom.y, min_zoom, max_zoom)
--- scripts/procedural_map.gd
+++ scripts/procedural_map.gd
@@ -4,45 +4,62 @@ extends Node2D
 
 var noise: Noise
 
-var width: int = 190
-var height: int = 108
+var width: int = 1000
+var height: int = 1000
+
+var chunk_size: int = 10
 
 var offset = Vector2i(0, 0)
 
 @onready var tile_map = $TileMap
 @onready var tile_set = tile_map.tile_set
+@export var loading_screen: Control
+@onready var progress_bar: ProgressBar
 
 var water_atlas = 17
 
 func _ready():
 	noise = noise_height_text.noise
-	generate_map()
+	loading_screen.visible = true
+	progress_bar = loading_screen.get_node("ProgressBar")
+	
+	progress_bar.value = 0
+	await generate_noise_chunks()  # Call the asynchronous map generation function
+	print("Map generation complete!")
 
-func generate_map():
-	# Utiliser des tableaux standards
-	var grass_water: Array[Vector2i] = []
-	var grass_hill: Array[Vector2i] = []
+func generate_noise_chunks():
+	var current: int = 0
+	var chunk_map: Array[Array] = []
 
-	# Précalculer les limites
-	var start_x = -int(width / 2)
-	var end_x = int(width / 2)
-	var start_y = -int(height / 2)
-	var end_y = int(height / 2)
+	for x in range(-width / 2, width / 2, chunk_size):
+		for y in range(-height / 2, height / 2, chunk_size):
+			chunk_map.append(make_chunk(x, y))
+	
+	await generate_layer(chunk_map, -2.0, 0, 0, 0, 3, 0)
+	await generate_layer(chunk_map, 0.0, 1, 0, 1, 3, 1)
+	await generate_layer(chunk_map, 0.1, 2, 0, 2, 3, 2)
 
-	for x in range(start_x, end_x):
-		for y in range(start_y, end_y):
-			var position = Vector2i(x, y) + offset
-			var noise_val = noise.get_noise_2d(x, y)
+func generate_layer(chunk_map: Array[Array], threshold: float, layer_id: int, terrain_set: int, terrain_id: int, step_size: int, step_number: int):
+	var chunk_nb = 1
+	var layer_size = chunk_map.size()
+	for chunk in  chunk_map:
+		var tile_chunk: Array[Vector2i] = []
+		for pos in chunk:
+			var noise_val = noise.get_noise_2d(pos.x - width / 2, pos.y - height / 2)
+			if (threshold == -2):
+				tile_map.set_cell(0, pos, water_atlas, Vector2i(0, 0))
+			
+			if (threshold != -2 && noise_val >= threshold):
+				tile_chunk.append(pos)
+		tile_map.set_cells_terrain_connect(layer_id, tile_chunk, terrain_set, terrain_id)
+		progress_bar.value = float( ( float(step_number) * float(layer_size) + float(chunk_nb) ) / ( float(layer_size) * float(step_size) ) ) * 100
+		
+		chunk_nb += 1
+		await get_tree().process_frame
 
-			# Placer l'eau
-			tile_map.set_cell(0, position, water_atlas, Vector2i(0, 0))
-
-			# Déterminer les types de terrain
-			if noise_val >= 0.0:
-				grass_water.append(position)
-			if noise_val >= 0.1:
-				grass_hill.append(position)
-
-	# Utiliser les appels de batch pour les terrains
-	tile_map.set_cells_terrain_connect(1, grass_water, 0, 0)
-	tile_map.set_cells_terrain_connect(2, grass_hill, 0, 1)
+func make_chunk(x: int, y: int) -> Array[Vector2i]:
+	var chunk: Array[Vector2i] = []
+	for cx in range(chunk_size):
+		for cy in range(chunk_size):
+			chunk.append(Vector2i(x + cx, y + cy))
+	return chunk
